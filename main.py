813943963a23e338501f546689d3dd5ac8184408
Justin Kay <14783039+Justinkay0@users.noreply.github.com>
2023-03-17 06:13:51 +0800

    Turtle challenge 1 drawing a square

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from turtle import Turtle, Screen
 
 thomas = Turtle()
+thomas.shape('turtle')
+thomas.color('crimson')
+
+for i in range(4):
+    thomas.forward(100)
+    thomas.right(90)
+
 screen = Screen()
 screen.exitonclick()
