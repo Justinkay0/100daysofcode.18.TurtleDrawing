79fc1defc7206d29ea3cd67fce32622b262ad790
Justin Kay <14783039+Justinkay0@users.noreply.github.com>
2023-03-17 06:53:16 +0800

    Turtle challenge 3: draw shapes

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from turtle import Turtle, Screen
+from random import randint
 
 thomas = Turtle()
+screen = Screen()
 thomas.shape('turtle')
 thomas.color('crimson')
+screen.colormode(255)
 
-for i in range(15):
-    thomas.pendown()
-    thomas.forward(10)
-    thomas.penup()
-    thomas.forward(10)
+colour = [randint(0, 255),
+          randint(0, 255),
+          randint(0, 255)]
 
-screen = Screen()
+for sides in range(3, 12):
+    rotation = 0
+    colour = [randint(0, 255),
+              randint(0, 255),
+              randint(0, 255)]
+    thomas.pencolor(colour)
+    while rotation != 360:
+        rotation += 360 / sides
+        if round(rotation) == 360:
+            rotation = 360
+        thomas.forward(30)
+        thomas.right(360 / sides)
+        print(rotation)
+
+
 screen.exitonclick()
