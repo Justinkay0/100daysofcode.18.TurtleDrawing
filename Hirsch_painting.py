b3f45442183117c8793b65f48ccea5512da784a1
Justinkay0 <14783039+Justinkay0@users.noreply.github.com>
2023-03-17 17:40:03 +0800

    Hirsch Painting 2 : drawing dots
    Signed-off-by: Justinkay0 <14783039+Justinkay0@users.noreply.github.com>

--- Hirsch_painting.py
+++ Hirsch_painting.py
@@ -1,4 +1,6 @@
 import colorgram
+import random
+import turtle
 
 color = colorgram.extract('hirst-painting.jpeg', 15)
 print(color)
@@ -11,4 +13,24 @@ for _ in color:
     color_input = tuple(color_input)
     color_list.append(color_input)
 
-print(color_list)
+
+def paint(tortoise, color_bank):
+    for _ in range(10):
+        tortoise.dot(20, random.choice(color_bank))
+        tortoise.forward(50)
+        tortoise.up()
+
+
+kratos = turtle.Turtle()
+screen = turtle.Screen()
+screen.colormode(255)
+kratos.up()
+screen.setworldcoordinates(-20, -20, 500, 300)
+
+zero = 0
+for i in range(10):
+    kratos.up()
+    paint(kratos, color_list)
+    kratos.setpos(0, zero + ((i + 1) * 30))
+
+screen.exitonclick()
